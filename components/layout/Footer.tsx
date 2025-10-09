ece722503d4e45ec7167342da98ff96f72167978
saulOMgit <sauloteromelchor@gmail.com>
2025-10-09 10:05:43 +0200

    add Footer

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,30 @@
+
+import React from 'react';
+import { GitHubIcon, LinkedInIcon, DocumentTextIcon } from '../icons/Icons';
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="bg-gray-100">
+            <div className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
+                <div className="flex flex-col items-center">
+                    <div className="flex space-x-6">
+                         <a href="https://github.com/saulOMgit/" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-primary-600 transition-colors" aria-label="GitHub">
+                            <GitHubIcon className="h-6 w-6" />
+                        </a>
+                        <a href="https://www.linkedin.com/in/sa%C3%BAl-otero-melchor-84b752282/" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-primary-600 transition-colors" aria-label="LinkedIn">
+                            <LinkedInIcon className="h-6 w-6" />
+                        </a>
+                        <a href="https://saulcv.netlify.app/" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-primary-600 transition-colors" aria-label="CV">
+                            <DocumentTextIcon className="h-6 w-6" />
+                        </a>
+                    </div>
+                    <p className="mt-4 text-center text-sm text-gray-500">
+                        Desarrollado por Saúl Otero Melchor
+                    </p>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
